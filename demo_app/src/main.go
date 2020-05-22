c30117f4085c65fc2fb03a803de24c5c52c9eebe
yarethpinzon <yarethpinzon@gmail.com>
2020-05-22 11:43:45 -0500

    Update main.go

--- demo_app/src/main.go
+++ demo_app/src/main.go
@@ -8,7 +8,7 @@ import (
 
 func mainHandler() http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World! (Version info: %s, build date: %s)", os.Getenv("VERSION_INFO"), os.Getenv("BUILD_DATE"))
+		fmt.Fprintf(w, "FUNCIONA YSP IN THE HOUSE MATHER FUCKERS! (Version info: %s, build date: %s)", os.Getenv("VERSION_INFO"), os.Getenv("BUILD_DATE"))
 	})
 }
 
